fe3c85f2337490f0de358a788ecbba0fb6c87757
rehanahmed0424 <62881626+rehanahmed0424@users.noreply.github.com>
2025-10-18 22:30:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,24 +11,22 @@ const nodemailer = require('nodemailer');
 
 // Initialize express app FIRST
 const app = express();
-const cors = require('cors');
 
 app.use(cors({
-  origin: ['https://sourcebd.vercel.app', 'https://sourcebd-deployment.vercel.app'], // your frontend URLs
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true
+  origin: [
+    'https://sourcebd.vercel.app',
+    'https://sourcebd-deployment.vercel.app',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 
 // Middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ 
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 // Add request logging
 app.use((req, res, next) => {
